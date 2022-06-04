7e6ad4a17b1c7f2ab99adbc324a1355ec55caf02
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-04 16:28:54 -0500

    changed dependencies in server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -32,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening @ http://localhost:${PORT}`));
