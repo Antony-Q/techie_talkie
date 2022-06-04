870156d31aefc7482c602a0c48ffe786458aa93c
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-04 16:57:14 -0500

    fixed sequelize

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
-
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
@@ -33,6 +31,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/js')));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening @ http://localhost:${PORT}`));
