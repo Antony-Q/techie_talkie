81af1713b2747d66e9357dcd38e26f726b6fab9a
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-04 15:25:58 -0500

    fixed hard PORT on server.js (2)

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening @ http://localhost:${PORT}`));
 });
